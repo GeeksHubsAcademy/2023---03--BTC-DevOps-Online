16eb17a3a18deb59c888ef0ca035c88e562da0e0
Nacho Carretero <nacho@sketch.com>
2023-04-20 19:54:38 +0200

    Session 5: Readmes and boilerplate

---
+++ Sesion5_IaC_Nacho/50_boilerplate/main.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ubuntu" {
+  owners      = ["099720109477"] # Amazon
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.selected.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [data.aws_security_group.allow_traffic.id]
+
+  tags = {
+    Name = "web_server"
+  }
+}
