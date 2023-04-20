808ba8cbb3605e0acea19f087cb1ca4c71c5139f
Nacho Carretero <nacho@sketch.com>
2023-04-20 20:37:00 +0200

    53 - Release latest AMI

--- Sesion5_IaC_Nacho/50_boilerplate/main.tf
+++ Sesion5_IaC_Nacho/50_boilerplate/main.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "ubuntu" {
-  owners      = ["099720109477"] # Amazon
+  owners      = ["self"]
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["web-server-*"]
   }
 }
 
